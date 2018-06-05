69f144cf8a02e3547d0d99c65f33291987f8db48
Rodrigo Ancavil del Pino <rancavil@gmail.com>
2018-06-05 10:26:17 -0400

    first version

---
+++ build/lib/simplewaze/config.py
@@ -0,0 +1,1 @@
+urlwaze='https://www.waze.com/row-rtserver/broadcast/BroadcastRSS?bid=795&format=JSON'
---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from setuptools import setup
+import os
+
+setup(
+    name = 'simplewaze',
+    version = 1.0,
+    author = 'Rodrigo Ancavil',
+    author_email = 'rancavil@gmail.com',
+    description = "Get route times",
+    license = 'GNU GPL v3',
+    keywords = ['waze', 'route', 'times'],
+    packages = ['simplewaze',]
+)
